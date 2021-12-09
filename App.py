c1a69d59f4db223f4e6c9d173f428639fe85e2f3
theordinarycode <ordinarycodeemail@gmail.com>
2021-12-10 06:01:22 +0700

    refine get_settings, rename_files, resize_images

--- App.py
+++ App.py
@@ -1,6 +1,9 @@
 from process_images import process_images
 from resize_image import resize_images, get_local_settings
 from get_settings import get_settings
+from rename_files import rename_files
 
 settings = get_settings()
+print(settings)
+rename_files(settings)
 resize_images(settings)
--- get_settings.py
+++ get_settings.py
@@ -1,10 +1,13 @@
 import os 
 import re
 
+
+settings = {}
+
 def get_settings():
     print('Will you be processing multiple images?')
     multiple = input('Type yes or no: ')
-    settings = {}
+    
     if multiple.lower() in ['yes', 'y'] :
         settings['multiple_files'] = True
         path = input('Path to your images: ')
@@ -13,19 +16,20 @@ def get_settings():
         formatted_directory = search[0] + '/'
         settings['path'] = formatted_directory 
         settings['number_of_files'] = len(os.listdir(path))
+         
+           
         print('Do you want to set a basename for the output files?')
         rename = input('type yes or no: ')
-        if rename.lower() in ['yes', 'y']:
-            settings['basename'] =input('Basename: ')
-        else:
-            first_pass = True
-            for file in os.listdir(settings['path']):
-                if not file.startswith('.') and first_pass == True:
-                    first_image = file
-                    #print(os.path.basename(first_image).split('.')[0])
-                    first_pass = False
-            settings['basename'] = os.path.basename(first_image).split('.')[0]
-    
+        first_pass = True
+        for file in os.listdir(settings['path']):
+            if not file.startswith('.') and first_pass == True:
+                first_image = file 
+                settings['file_extension']=first_image.split(".")[-1]
+                #print(os.path.basename(first_image).split('.')[0])
+                settings['basename'] = os.path.basename(first_image).split('.')[0]
+                if rename.lower() in ['yes', 'y']:
+                    settings['basename'] =input('Basename: ')
+                first_pass = False
 
     else:
         settings['multiple_files'] = False
@@ -39,8 +43,12 @@ def get_settings():
         else:
             settings['basename']=os.path.basename(path).split('.')[0]
         settings['file_extension']=os.path.basename(path).split(".")[-1]
+        settings['number_of_files'] = 1
 
     output_directory = input('output directory: ')
-    settings['output_directory'] = output_directory
+    regex = r'.+[^/]'
+    search = re.findall(regex, output_directory)
+    formatted_directory = search[0] + '/'
+    settings['output_directory'] = formatted_directory
     
     return settings
